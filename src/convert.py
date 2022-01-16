a02506bd14ea6a9e8ef2be58ec662d71e1db4aba
Jessica Becerra <jessicabefor@gmail.com>
2022-01-16 17:39:50 +0100

    Create convert.py, add to_decimal()

---
+++ src/convert.py
@@ -0,0 +1,17 @@
+from typing import Union
+
+
+def to_decimal(number: Union[int, str], base: int) -> int:
+    highest_digit_exp = len(str(number)) - 1
+    decimal_number = 0
+
+    hex_mappings = {"A": 10, "B": 11, "C": 12, "D": 13, "E": 14, "F": 15}
+
+    for digit in str(number):
+        if base == 16:
+            if digit in hex_mappings.keys():
+                digit = hex_mappings[digit]
+        decimal_number += int(digit) * (base ** highest_digit_exp)
+        highest_digit_exp -= 1
+
+    return decimal_number
