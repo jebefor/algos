67b9c62b4fd0106a5747738b9c9a653b9df44585
Jessica Becerra <jessicabefor@gmail.com>
2022-01-16 11:31:16 +0100

    Add selection sort

--- src/sort.py
+++ src/sort.py
@@ -12,4 +12,17 @@ def bubble_sort(array):
 
   return array
 
+def selection_sort(array):
+  # O(N^2). Actually O(N^2/2), but Big O Notation doesn't care about constants...
+  first_idx_to_sort = 0
+  last_idx_to_sort = len(array)
+  
+  while first_idx_to_sort < last_idx_to_sort:
+    idx_lowest_num = first_idx_to_sort
+    for idx in range(first_idx_to_sort+1, last_idx_to_sort):
+      if array[idx] < array[idx_lowest_num]:
+        idx_lowest_num = idx
+    array[first_idx_to_sort], array[idx_lowest_num] = array[idx_lowest_num], array[first_idx_to_sort]
+    first_idx_to_sort += 1
 
+  return array
